abd2873c40b09734c22b101e5b6d3701c6211b80
Jihee Ryu <chrisjihee@etri.re.kr>
2017-08-29 11:07:09 +0900

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <cstdio>
+
+#include "MathFunctions.h"
+
+int main() {
+    double inputValue = 3.0;
+    printf("The square root of %g is %g\n", inputValue, mysqrt(inputValue));
+    return 0;
+}
