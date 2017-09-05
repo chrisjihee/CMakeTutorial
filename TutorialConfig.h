8c5fb00e9d0d65074f1d0ecec8c0d81e73f7300f
Jihee Ryu <chrisjihee@etri.re.kr>
2017-09-05 16:29:44 +0900

    Add setting: CMake 옵션 값 확인 기능 추가!!

---
+++ TutorialConfig.h
@@ -0,0 +1,6 @@
+#define Tutorial_VERSION_MAJOR  1
+#define Tutorial_VERSION_MINOR  0
+#define CMAKE_CXX_FLAGS_DEBUG   "-g"
+#define CMAKE_CXX_FLAGS_RELEASE "-O3 -DNDEBUG"
+#define CMAKE_C_FLAGS_DEBUG     "-g"
+#define CMAKE_C_FLAGS_RELEASE   "-O3 -DNDEBUG"
--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
 #include <cstdio>
 
 #include "MathFunctions.h"
+#include "TutorialConfig.h"
 
 int main() {
+    printf("================================================================================\n");
+    printf("CMAKE_CXX_FLAGS_DEBUG = %s\n", CMAKE_CXX_FLAGS_DEBUG);
+    printf("CMAKE_CXX_FLAGS_RELEASE = %s\n", CMAKE_CXX_FLAGS_RELEASE);
+    printf("CMAKE_C_FLAGS_DEBUG = %s\n", CMAKE_C_FLAGS_DEBUG);
+    printf("CMAKE_C_FLAGS_RELEASE = %s\n", CMAKE_C_FLAGS_RELEASE);
+    printf("================================================================================\n");
+
     double inputValue = 3.0;
     printf("The square root of %g is %g\n", inputValue, mysqrt(inputValue));
+    printf("================================================================================\n");
+
     return 0;
 }
